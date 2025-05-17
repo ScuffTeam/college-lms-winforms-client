593afbc71d5194bdf594144b33245b122e3a10d7
prrek <prrek2971@gmail.com>
2025-05-17 13:54:27 +0300

    добавнены DTO модели для расписания

---
+++ dtos/AttenanceMark.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace college_lms_winforms_client.dtos;
+public class AttendanceMarkDto
+{
+    public int Id { get; set; }
+
+    [Required]
+    public int StudentId { get; set; }
+
+    [Required]
+    public int LessonId { get; set; }
+
+    [Required]
+    [Range(0, 100, ErrorMessage = "Оценка должна быть в диапазоне от 0 до 100")]
+    public int GradeValue { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
